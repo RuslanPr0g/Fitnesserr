8eb4ccff50984eabd09902f0f53cab8274ec7a45
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-23 12:16:29 +0300

    add training-done-create validator

---
+++ fitnesserr-server/WEBApi/Validators/TrainingDoneValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEBApi.DTOs;
+
+namespace WEBApi.Validators
+{
+    public class TrainingDoneValidator : AbstractValidator<TrainingDoneCreateDto>
+    {
+        public TrainingDoneValidator()
+        {
+            RuleFor(e => e.TimeDone).InclusiveBetween(0, 9000).WithMessage("Too long, if user cheated, please, consider lower the time to zero.");
+        }
+    }
+}
