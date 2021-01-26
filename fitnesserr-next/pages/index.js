f5ed6f6d8ae64c8bda2389ef337fa1ed25670ce5
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-26 15:48:53 +0300

    add main page structure

--- fitnesserr-next/pages/index.js
+++ fitnesserr-next/pages/index.js
@@ -1,32 +1,28 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout home>
       <Head>
-        <title>Fitnesserr</title>
+        <title>{siteTitle}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to FITNESSERR, guest, see our{" "}
-          <Link href="/trainings/exercises">
-            <a className="link">Exercises</a>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Check out{" "}
+          <Link href="/exercise">
+            <a>exercises</a>
           </Link>{" "}
-          and{" "}
-          <Link href="/users/signup">
-            <a>SignUP</a>
+          or{" "}
+          <Link>
+            <a>sign up</a>
           </Link>{" "}
-          for free!
-        </h1>
-        <br />
-        <br />
-        <br />
-        <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-      </main>
-    </div>
+          for free to get started your routine!
+        </p>
+      </section>
+    </Layout>
   );
 }
